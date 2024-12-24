5c5e5e9b1d56f0a119f7085d9f521b46131ecf22
SotaSill <sotasill@gmail.com>
2024-12-24 13:18:16 +0200

    ver

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,26 +1,27 @@
 import { ContactsCollection } from '../db/models/contacts.js';
 
 export const getAllContacts = async () => {
-  const contacts = await ContactsCollection.find();
+  const contacts = await ContactsCollection.find().select('-__v');
   return contacts;
 };
 
 export const getContactById = async (contactId) => {
-  const contact = await ContactsCollection.findById(contactId);
+  const contact = await ContactsCollection.findById(contactId).select('-__v');
   return contact;
 };
 
 export const createContact = async (contactData) => {
   const newContact = new ContactsCollection(contactData);
-  return await newContact.save();
+  const savedContact = await newContact.save();
+  return savedContact.toObject({ versionKey: false });
 };
 
 export const deleteContact = async (contactId) => {
-  const deletedContact = await ContactsCollection.findByIdAndDelete(contactId);
+  const deletedContact = await ContactsCollection.findByIdAndDelete(contactId).select('-__v');
   return deletedContact;
 };
 
 export const updateContact = async (contactId, updateData) => {
-  const updatedContact = await ContactsCollection.findByIdAndUpdate(contactId, updateData, { new: true });
+  const updatedContact = await ContactsCollection.findByIdAndUpdate(contactId, updateData, { new: true }).select('-__v');
   return updatedContact;
 };
