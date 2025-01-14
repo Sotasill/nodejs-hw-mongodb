9a1bf42042fa7b6dd0058b1dcf0ffeee9622ce15
SotaSill <sotasill@gmail.com>
2025-01-14 20:19:42 +0200

    docs

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import swaggerUi from 'swagger-ui-express';
+import { readFileSync } from 'fs';
 import contactsRouter from './routers/contacts.js';
 import authRouter from './routers/auth.js';
 import errorHandler from './middlewares/errorHandler.js';
@@ -9,6 +11,9 @@ import notFoundHandler from './middlewares/notFoundHandler.js';
 import { getEnvVar } from './utils/getEnvVar.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
+const swaggerDocument = JSON.parse(
+  readFileSync('./docs/swagger.json', 'utf-8'),
+);
 
 export const setupServer = () => {
   const app = express();
@@ -22,6 +27,7 @@ export const setupServer = () => {
   );
   app.use(cookieParser());
 
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
 
